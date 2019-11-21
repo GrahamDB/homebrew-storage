88cf55c71527b34ebd8aa6b46ee30f0e2e88682a
Graham Derryberry <g.derryberry@gmail.com>
2019-11-21 12:00:02 -0500

    Modifications to use custom branch of s3fs-fuse

--- Formula/s3fs-rgw.rb
+++ Formula/s3fs-rgw.rb
@@ -1,9 +1,9 @@
-class S3fs < Formula
-  desc "FUSE-based file system backed by Amazon S3"
+class S3fsRGW < Formula
+  desc "FUSE-based file system backed by Amazon S3, modified for "
   homepage "https://github.com/s3fs-fuse/s3fs-fuse/wiki"
-  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.85.tar.gz"
+  url url "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.85.tar.gz"
   sha256 "c4b48c0aba6565b9531c251d42a6a475a7e845909a3017b61d9c945b15cc008f"
-  head "https://github.com/s3fs-fuse/s3fs-fuse.git"
+  head "https://github.com/GrahamDB/s3fs-fuse.git", :branch => "osx_iosize" 
 
   bottle do
     cellar :any
@@ -12,6 +12,7 @@ class S3fs < Formula
     sha256 "c9f8045bd8cf246306103400cfe191e92a41d52baccd7ab9596dd80cdaa30237" => :high_sierra
     sha256 "37952aa8b45492b8fcd4b3c468b5c72ceec9407b1126a79c1cbf4f0a9f0e878b" => :sierra
   end
+  conflicts_with "s3fs", :because => "because both install 's3fs' binaries"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
